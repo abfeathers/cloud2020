fd3463a69d5c8105b25b983561850a2ae10651a9
abfeathers <abfeathers@qq.com>
2020-06-04 10:12:44 +0800

    完成nacos+nginx集群部署

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/abfeather/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.abfeather.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Abfeathers
+ * @date 2020/5/15 11:21
+ * @Description
+ *
+ */
+@RestController
+@RefreshScope   //SpringCloud 原生注解， 支持nacos的动态刷新功能
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
