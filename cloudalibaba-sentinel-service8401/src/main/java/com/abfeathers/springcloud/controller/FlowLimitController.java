de2f6505ff6ba88ab886816b860ea4498809b771
abfeathers <abfeathers@qq.com>
2020-06-04 17:41:22 +0800

    增加sentinel功能

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/abfeathers/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,26 @@
+package com.abfeathers.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author Abfeathers
+ * @date $ $
+ * @Description:
+ */
+@RestController
+@Slf4j
+public class FlowLimitController {
+    @GetMapping("/testA")
+    public String testA(){
+        return "--------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB(){
+        log.info(Thread.currentThread().getName() + "\t" + "....testB");
+        return "--------testB";
+    }
+}
